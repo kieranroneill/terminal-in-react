0f195450e5bda18243398d1f9dc9beb654acd872
Kieran O'Neill <kieran.oneill@protonmail.com>
2020-11-26 11:13:27 +0100

    Fix: Remove default help command

--- src/js/args/parse.js
+++ src/js/args/parse.js
@@ -4,22 +4,22 @@ export default function (argv, options) {
   // Override default option values
   Object.assign(this.config, options);
 
-  if (this.config.help) {
-    // Register default options and commands
-    this.option('help', 'Output usage information');
-    this.command('help', 'Display help', this.showHelp);
-  }
+  // if (this.config.help) {
+  //   // Register default options and commands
+  //   this.option('help', 'Output usage information');
+  //   this.command('help', 'Display help', this.showHelp);
+  // }
 
   // Parse arguments using minimist
   this.raw = parser(argv.slice(1), this.config.minimist);
 
   // If default version is allowed, check for it
-  if (this.config.version) {
-    this.checkVersion(this.parent);
-  }
+  // if (this.config.version) {
+  //   this.checkVersion(this.parent);
+  // }
 
   const subCommand = this.raw._[1];
-  const helpTriggered = this.raw.h || this.raw.help;
+  // const helpTriggered = this.raw.h || this.raw.help;
 
   const args = {};
   const defined = this.isDefined(subCommand, 'commands');
@@ -39,9 +39,9 @@ export default function (argv, options) {
 
   // Show usage information if "help" or "h" option was used
   // And respect the option related to it
-  if (this.config.help && helpTriggered) {
-    this.showHelp();
-  }
+  // if (this.config.help && helpTriggered) {
+  //   this.showHelp();
+  // }
 
   // Hand back list of options
   return optionList;
